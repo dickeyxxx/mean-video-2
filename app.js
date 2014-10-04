52fef37f22e904826138d56eabfb330cc4aba66e
dickeyxxx <jeff@dickeyxxx.com>
2014-10-04 16:53:11 -0700

    voting app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 angular.module('myapp', [])
+
+angular.module('myapp')
+.controller('VotingCtrl', function ($scope) {
+  $scope.numberOfYes = 0
+  $scope.numberOfNo = 0
+
+  $scope.voteYes = function () {
+    $scope.numberOfYes++
+  }
+
+  $scope.voteNo = function () {
+    $scope.numberOfNo++
+  }
+})
